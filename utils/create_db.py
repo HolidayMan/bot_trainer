4f788ce2e12f7e2334d8eee026ee8e8b6b8ea916
HolidayMan <lesha.pitenko791@gmail.com>
2019-08-04 15:10:14 +0300

    small changes

--- utils/create_db.py
+++ utils/create_db.py
@@ -1,7 +1,6 @@
 import os, sys
 sys.path.append(os.path.dirname(os.getcwd()))
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from models.chat_model import Chat
 from models.plan_model import Plan
 from models.user_model import User
@@ -14,6 +13,3 @@ except ImportError:
 
 engine = DB_ENGINE
 Base.metadata.create_all(bind=engine)
-
-
-

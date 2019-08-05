7c3d09f7d7eb183c2239d52d696fa7ec73acb571
HolidayMan <lesha.pitenko791@gmail.com>
2019-08-05 14:38:39 +0300

    moved, renamed, added PlanDB class

---
+++ core/db.py
@@ -0,0 +1,113 @@
+from sqlalchemy.orm import sessionmaker
+from models.plan_model import Plan
+from models.user_model import User
+
+
+try:
+    from local_settings.config import DB_ENGINE
+except ImportError:
+    from prod_settings.config import DB_ENGINE
+
+
+engine = DB_ENGINE
+Session = sessionmaker(bind=engine)
+
+
+class ObjectDB:
+    session = Session()
+
+    def print_sess(self):
+        print(id(self.session))
+
+    def close_session(self):
+        self.session.close()
+
+
+class UserDB(ObjectDB):
+    
+    def __init__(self, tguserobj):
+        self.user = self.session.query(User).filter(User.tg_id == tguserobj.id).first()
+        if not self.user: # user isn't in db
+            self.user = self.create(tguserobj)
+        if self.user.first_name != tguserobj.first_name or self.user.username != tguserobj.username:
+            self.update(tguserobj)
+            
+    
+    def create(self, tguserobj):
+        new_user = User(tg_id=tguserobj.id, first_name=tguserobj.first_name, username=tguserobj.username)
+        self.session.add(new_user)
+        self.session.commit()
+        return new_user
+
+
+    def update(self, tguserobj):
+        self.user.first_name = tguserobj.first_name
+        self.user.username = tguserobj.username
+        self.session.commit()
+
+
+    def delete(self):
+        self.session.delete(self.user)
+        self.session.commit()
+        del self
+
+    
+    @staticmethod
+    def get_all_users():
+        session = Session()
+        users = session.query(User).all()
+        session.close()
+        return users
+
+
+class PlanDB(ObjectDB):
+    def __init__(self, user_db_object=None, instance=None):
+        if user_db_object:
+            self.user = user_db_object.user
+        elif instance:
+            self.plan = instance
+
+
+    def create(self, title, type, user=None):
+        new_plan = Plan(title=title, type=type, status=Plan.STATUS_WAIT, user=self.user if not user else user)
+        self.session.add(new_plan)
+        self.session.commit()
+        return new_plan
+
+    
+    def get_all_plans(self, status=None, type=None):
+        plans_query = self.session.query(Plan).join(Plan.user).filter(User.id == self.user.id)
+        if status:
+            plans_query = plans_query.filter(Plan.status == status)
+        if type:
+            plans_query = plans_query.filter(Plan.type == type)
+        return plans_query.order_by(Plan.status).all()
+    
+
+    def mark_canceled(self):
+        if not self.plan:
+            return
+        self.plan.status = Plan.STATUS_CANCELED
+        self.session.commit()
+
+    
+    def mark_done(self):
+        if not self.plan:
+            return
+        self.plan.status = Plan.STATUS_DONE
+        self.session.commit()
+    
+
+    def mark_overdue(self):
+        if not self.plan:
+            return
+        if self.plan.is_overdue():
+            self.plan.status = Plan.STATUS_OVERDUE
+        self.session.commit()
+
+
+    def mark_wait(self):
+        if not self.plan:
+            return
+        self.plan.status = Plan.STATUS_WAIT
+        self.session.commit()

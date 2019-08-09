dd3f85b3dcc9481303bfe7b9da9b7cbbc16a3c17
HolidayMan <lesha.pitenko791@gmail.com>
2019-08-09 23:13:44 +0300

    created

---
+++ bot/plans/plan_buffer.py
@@ -0,0 +1,26 @@
+import pickle
+
+class PlanBuffer:
+    filetosave = "plan_buffer"
+    buffer = {}
+
+
+    def __init__(self):
+        self.upload()
+
+
+    def save(self):
+        with open(self.filetosave, 'wb') as file:
+            pickle.dump(self.buffer, file)
+    
+    
+    def upload(self):
+        try:
+            with open(self.filetosave, 'rb') as file:
+                self.buffer = pickle.load(file)
+        except FileNotFoundError:
+            self.buffer = {}
+    
+
+    def __del__(self):
+        self.save()

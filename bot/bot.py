64f0d80ffe892f7a6da335162c7e32ae1f62daf4
HolidayMan <lesha.pitenko791@gmail.com>
2019-08-10 21:51:55 +0300

    basic commands were created

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,22 @@
 import telebot
-import sys
+from bot.states.base_states import States
+from core.db import UserDB, set_state
 try:
     import local_settings.config as config
 except ModuleNotFoundError:
     import prod_settings.config as config
 
 bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def cmd_start(message):
+    HELP_MESSAGE = "It's a bot"
+    UserDB(message.chat)
+    bot.send_message(message.chat.id, HELP_MESSAGE)
+
+
+@bot.message_handler(commands=['cancel'])
+def cmd_cancel(message):
+    set_state(message.chat.id, States.S_ENTERCOMMAND.value)
+

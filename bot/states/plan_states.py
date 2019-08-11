9215c4996e9e17641bae891295496e1456b55a20
HolidayMan <lesha.pitenko791@gmail.com>
2019-08-11 17:08:32 +0300

    added a few states

--- bot/states/plan_states.py
+++ bot/states/plan_states.py
@@ -3,3 +3,6 @@ from enum import Enum
 class PlanStates(Enum):
     S_NEWCHOOSETYPE = "planstates.newchoosetype"
     S_NEWENTERTITLE = "planstates.newentertitle"
+    S_EDITCHOOSETYPE = "planstates.editchoosetype"
+    S_EDITCHOOSEPLAN = "planstates.edidchooseplan"
+    S_EDITPLAN = "planstate.editplan"

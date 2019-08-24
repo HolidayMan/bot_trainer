4aef5898f3a1773ce934a477d7bcc972bda64286
HolidayMan <lesha.pitenko791@gmail.com>
2019-08-25 02:45:17 +0300

    added threading for scheduler

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from bot.bot import bot
-
 from bot.plans.handlers import *
+from bot.schedulers import *
+import threading
+import schedule
+import time
 
-bot.polling(none_stop=True)
+def check_schedulers():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+poll_thread = threading.Thread(target=bot.polling, kwargs={'none_stop': True}, daemon=True)
+schedule_thread = threading.Thread(target=check_schedulers, daemon=True)
+
+poll_thread.start()
+schedule_thread.start()
+poll_thread.join()

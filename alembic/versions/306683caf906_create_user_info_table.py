64b6d1f2279945660e778d42153c690b4d5f9ce0
HolidayMan <lesha.pitenko791@gmail.com>
2019-10-05 23:42:41 +0300

    added alembic migrations, created first migration

---
+++ alembic/versions/306683caf906_create_user_info_table.py
@@ -0,0 +1,36 @@
+"""create user_info table
+
+Revision ID: 306683caf906
+Revises: 
+Create Date: 2019-10-05 22:50:45.747356
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from datetime import timezone
+
+# revision identifiers, used by Alembic.
+revision = '306683caf906'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'user_info',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(64), nullable=False, unique=False),
+        sa.Column('surname', sa.String(64), nullable=False, unique=False),
+        sa.Column('age', sa.Integer(), nullable=False, unique=False),
+        sa.Column('planning_time', sa.Time(), nullable=False, unique=False),
+        sa.Column('question1', sa.String(64), nullable=False, unique=False),
+        sa.Column('question2', sa.String(64), nullable=False, unique=False),
+        sa.Column('question3', sa.String(64), nullable=False, unique=False),
+        sa.Column('question4', sa.String(64), nullable=False, unique=False),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+    )
+
+
+def downgrade():
+   op.drop_table('user_info')
